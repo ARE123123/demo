ac3b458a7aa33beb4fa2daa42c9b3d3cd2c10598
ARE123123 <56105589+ARE123123@users.noreply.github.com>
2022-07-15 22:12:59 +0800

    feat:新增文件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="three" class="container"></div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import * as THREE from 'three';
+import * as maptalks from 'maptalks';
+import { ThreeLayer } from 'maptalks.three';
+import { onMounted } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+onMounted(() => {
+  var map = new maptalks.Map('three', {
+    center: [-0.113049, 51.498568],
+    zoom: 14,
+    baseLayer: new maptalks.TileLayer('base', {
+      urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+      subdomains: ['a', 'b', 'c', 'd'],
+      attribution:
+        '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>',
+    }),
+  });
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0px;
+  height: 100%;
+  width: 100%;
+}
+
+#app,
+.container {
+  width: 100%;
+  height: 100%;
 }
 </style>
